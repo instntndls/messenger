f571cdff76406840a107b094419c57f905a84993
instntndls <farn7229@gmail.com>
2024-03-03 20:21:31 +0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/messenger/',
+  server: {
+    host: 'https://instntndls.github.io',
+  },
   css: {
     postcss: {
       plugins: [tailwind(), autoprefixer()],
