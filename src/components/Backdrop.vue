0c49a70a1e1370b709c3533c7f529c5623ebffc0
instntndls <farn7229@gmail.com>
2024-03-03 22:20:02 +0500

    fix

--- src/components/Backdrop.vue
+++ src/components/Backdrop.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <img class="fixed top-0 left-0 w-screen h-screen z-0 select-none brightness-75" src="/src/assets/giphy.gif" alt="">
+  <img class="fixed top-0 left-0 w-screen h-screen z-0 select-none brightness-50" src="/src/assets/giphy.gif" alt="">
   <div class="fixed top-0 left-0 w-screen h-screen backdrop-blur-2xl z-[1] select-none"/>
 </template>
 
